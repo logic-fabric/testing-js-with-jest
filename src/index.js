3e4772206cb8fde6fa05b491540411900a96c82c
logic-fabric <lowic@logic-fabric.fr>
2022-06-19 10:06:26 +0200

    feature: retrieve app config from course codebase

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+ReactDOM.render(<h1>React Calculator</h1>, document.getElementById("app"));
