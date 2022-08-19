479d209e8af1c816fd0eb83575eaa0cb93e054fd
logic-fabric <lowic@logic-fabric.fr>
2022-08-19 17:21:09 +0200

    test: configure Jest to handle CSS while rendering

---
+++ src/components/atoms/CalculatorDisplay.spec.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+
+import { CalculatorDisplay } from "./CalculatorDisplay.jsx";
+
+test("CalculatorDisplay renders", () => {
+  render(<CalculatorDisplay />);
+});
